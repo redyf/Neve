0d0260d0453ed210728fc38716da07275566d024
Redyf <mateusalvespereira7@gmail.com>
2024-01-19 19:43:52 -0300

    feat: Add charslist to Neve + improve indent-blankline

--- config/sets.nix
+++ config/sets.nix
@@ -3,72 +3,77 @@
 , config
 , ...
 }: {
-  globalOptions = {
-    # Block cursor
-    guicursor = "n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor";
-    cursorline = false; # Highlight the line where the cursor is located
+  config = {
+    options = {
+      # Block cursor
+      guicursor = "n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor";
+      cursorline = false; # Highlight the line where the cursor is located
 
-    # Enable relative line numbers
-    number = true;
-    relativenumber = true;
+      # Enable relative line numbers
+      number = true;
+      relativenumber = true;
 
-    # Tab spacing
-    tabstop = 2; # insert 4 spaces for a tab
-    softtabstop = 2;
-    shiftwidth = 2; # insert 4 spaces for each indentation
-    expandtab = true;
+      # Tab spacing
+      tabstop = 2; # insert 4 spaces for a tab
+      softtabstop = 2;
+      shiftwidth = 2; # insert 4 spaces for each indentation
+      expandtab = true;
 
-    smartindent = true;
+      smartindent = true;
 
-    wrap = true;
+      wrap = true;
 
-    # Undo stuff from days ago
-    swapfile = false;
-    backup = false;
-    undofile = true;
+      # Undo stuff from days ago
+      swapfile = false;
+      backup = false;
+      undofile = true;
 
-    # Incremental search
-    hlsearch = false;
-    incsearch = true;
+      # Incremental search
+      hlsearch = false;
+      incsearch = true;
 
-    # Better colors
-    termguicolors = false;
+      # Better colors
+      termguicolors = true;
 
-    scrolloff = 8; # Will never have less than 8 characters as you scroll down
-    signcolumn = "yes";
-    updatetime = 50; # faster completion (4000ms default)
-    colorcolumn = "80";
-    mouse = "a"; # Mouse
-    timeoutlen = 10;
-    encoding = "utf-8";
-    fileencoding = "utf-8";
+      scrolloff = 8; # Will never have less than 8 characters as you scroll down
+      signcolumn = "yes";
+      updatetime = 50; # faster completion (4000ms default)
+      colorcolumn = "80";
+      mouse = "a"; # Mouse
+      timeoutlen = 10;
+      encoding = "utf-8";
+      fileencoding = "utf-8";
+      # Enable chars list
+      list = true;
+      listchars = "eol:↲,tab:|->,lead:·,space: ,trail:•,extends:→,precedes:←,nbsp:␣";
+    };
+    extraConfigLua = ''
+      local opt = vim.opt
+      local g = vim.g
+      local o = vim.o
+        -- Neovide
+      if g.neovide then
+        -- Neovide options
+        g.neovide_fullscreen = false
+        g.neovide_hide_mouse_when_typing = false
+        g.neovide_refresh_rate = 165
+        g.neovide_cursor_vfx_mode = "ripple"
+        g.neovide_cursor_animate_command_line = true
+        g.neovide_cursor_animate_in_insert_mode = true
+        g.neovide_cursor_vfx_particle_lifetime = 5.0
+        g.neovide_cursor_vfx_particle_density = 14.0
+        g.neovide_cursor_vfx_particle_speed = 12.0
+        g.neovide_transparency = 0.8
+
+        -- Neovide Fonts
+        o.guifont = "MonoLisa Trial:Medium:h15"
+        -- o.guifont = "CommitMono:Medium:h15"
+        -- o.guifont = "JetBrainsMono Nerd Font:h14:Medium:i"
+        -- o.guifont = "FiraMono Nerd Font:Medium:h14"
+        -- o.guifont = "CaskaydiaCove Nerd Font:h14:b:i"
+        -- o.guifont = "BlexMono Nerd Font Mono:h14:Medium:i"
+        -- o.guifont = "Liga SFMono Nerd Font:b:h15"
+      end
+    '';
   };
-  extraConfigLua = ''
-    local opt = vim.opt
-    local g = vim.g
-    local o = vim.o
-      -- Neovide
-    if g.neovide then
-      -- Neovide options
-      g.neovide_fullscreen = false
-      g.neovide_hide_mouse_when_typing = false
-      g.neovide_refresh_rate = 165
-      g.neovide_cursor_vfx_mode = "ripple"
-      g.neovide_cursor_animate_command_line = true
-      g.neovide_cursor_animate_in_insert_mode = true
-      g.neovide_cursor_vfx_particle_lifetime = 5.0
-      g.neovide_cursor_vfx_particle_density = 14.0
-      g.neovide_cursor_vfx_particle_speed = 12.0
-      g.neovide_transparency = 0.8
-
-      -- Neovide Fonts
-      o.guifont = "MonoLisa Trial:Medium:h15"
-      -- o.guifont = "CommitMono:Medium:h15"
-      -- o.guifont = "JetBrainsMono Nerd Font:h14:Medium:i"
-      -- o.guifont = "FiraMono Nerd Font:Medium:h14"
-      -- o.guifont = "CaskaydiaCove Nerd Font:h14:b:i"
-      -- o.guifont = "BlexMono Nerd Font Mono:h14:Medium:i"
-      -- o.guifont = "Liga SFMono Nerd Font:b:h15"
-    end
-  '';
 }
