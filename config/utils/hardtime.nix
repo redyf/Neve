0bedb9ae9944d7045cb163413f1e0c150ececc87
Redyf <mateusalvespereira7@gmail.com>
2024-01-27 21:35:56 -0300

    feat: Add hardtime.nvim plugin

---
+++ config/utils/hardtime.nix
@@ -0,0 +1,10 @@
+{
+  plugins.hardtime = {
+    enable = true;
+    disableMouse = true;
+    disabledFiletypes = [ "neo-tree" "qf" "netrw" "NvimTree" ];
+    hint = true;
+    maxCount = 4;
+    maxTime = 1000;
+  };
+}
