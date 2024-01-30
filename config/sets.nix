7ccd7fd2d07a2317f46a10da2b030617feda345f
Redyf <mateusalvespereira7@gmail.com>
2024-01-30 19:39:46 -0300

    refactor: Added new options + comments to understand them

--- config/sets.nix
+++ config/sets.nix
@@ -4,58 +4,110 @@
 , ...
 }: {
   config = {
-    # TODO: Organize the options
     options = {
-      cmdheight = 2; # more space in the neovim command line for displaying messages
-      completeopt = [ "menuone" "noselect" "noinsert" ]; # mostly just for cmp
-      showmode = false; # we don't need to see things like INSERT anymore
-      # Block cursor
-      guicursor = "n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor";
-      cursorline = true; # Highlight the line where the cursor is located
-
       # Enable relative line numbers
       number = true;
       relativenumber = true;
 
-      # Tab spacing
-      tabstop = 2; # insert 4 spaces for a tab
+      # Set tabs to 2 spaces 
+      tabstop = 2;
       softtabstop = 2;
-      shiftwidth = 2; # insert 4 spaces for each indentation
       showtabline = 2;
       expandtab = true;
 
+      # Enable auto indenting and set it to spaces
       smartindent = true;
+      shiftwidth = 2;
 
-      wrap = true;
+      # Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
+      breakindent = true;
 
-      # Undo stuff from days ago
-      swapfile = false;
-      backup = false;
-      undofile = true;
-
-      # Search
+      # Enable incremental searching
       hlsearch = true;
       incsearch = true;
+
+      # Enable text wrap
+      wrap = true;
+
+      # Better splitting
+      splitbelow = true;
+      splitright = true;
+
+      # Enable mouse mode
+      mouse = "a"; # Mouse
+
+      # Enable ignorecase + smartcase for better searching
       ignorecase = true;
       smartcase = true; # Don't ignore case with capitals
       grepprg = "rg --vimgrep";
       grepformat = "%f:%l:%c:%m";
 
-      # Better colors
+      # Decrease updatetime
+      updatetime = 50; # faster completion (4000ms default)
+
+      # Set completeopt to have a better completion experience
+      completeopt = [ "menuone" "noselect" "noinsert" ]; # mostly just for cmp
+
+      # Enable persistent undo history
+      swapfile = false;
+      backup = false;
+      undofile = true;
+
+      # Enable 24-bit colors 
       termguicolors = true;
 
-      scrolloff = 8; # Will never have less than 8 characters as you scroll down
+      # Enable the sign column to prevent the screen from jumping
       signcolumn = "yes";
-      updatetime = 50; # faster completion (4000ms default)
+
+      # Enable cursor line highlight
+      cursorline = true; # Highlight the line where the cursor is located
+
+      # Set fold settings
+      # These options were reccommended by nvim-ufo
+      # See: https://github.com/kevinhwang91/nvim-ufo#minimal-configuration
+      foldcolumn = "0";
+      foldlevel = 99;
+      foldlevelstart = 99;
+      foldenable = true;
+
+      # Always keep 8 lines above/below cursor unless at start/end of file
+      scrolloff = 8;
+
+      # Place a column line
       colorcolumn = "80";
-      mouse = "a"; # Mouse
+
+      # Reduce which-key timeout to 10ms
       timeoutlen = 10;
+
+      # Set encoding type
       encoding = "utf-8";
       fileencoding = "utf-8";
+
+      # Change cursor options
+      guicursor = [
+        "n-v-c:block" # Normal, visual, command-line: block cursor
+        "i-ci-ve:block" # Insert, command-line insert, visual-exclude: vertical bar cursor with block cursor, use "ver25" for 25% width
+        "r-cr:hor20" # Replace, command-line replace: horizontal bar cursor with 20% height
+        "o:hor50" # Operator-pending: horizontal bar cursor with 50% height
+        "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor" # All modes: blinking settings
+        "sm:block-blinkwait175-blinkoff150-blinkon175" # Showmatch: block cursor with specific blinking settings
+      ];
+
+      # Block cursor
+      # guicursor = "n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor";
+
       # Enable chars list
       list = true; # Show invisible characters (tabs, eol, ...)
       listchars = "eol:↲,tab:|->,lead:·,space: ,trail:•,extends:→,precedes:←,nbsp:␣";
+
+      # More space in the neovim command line for displaying messages
+      cmdheight = 2;
+
+      # We don't need to see things like INSERT anymore
+      showmode = false;
+
     };
+
     extraConfigLua = ''
       local opt = vim.opt
       local g = vim.g
