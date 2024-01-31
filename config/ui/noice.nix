fc4486eb75a3623a52ce499ab3ad98155dfb32f4
Redyf <mateusalvespereira7@gmail.com>
2024-01-31 12:21:37 -0300

    feat: Add new config for nvim-notify

--- config/ui/noice.nix
+++ config/ui/noice.nix
@@ -1,6 +1,15 @@
 {
   plugins.noice = {
     enable = true;
+    lsp = {
+      message = {
+        enabled = true;
+      };
+      progress = {
+        enabled = true;
+        view = "mini";
+      };
+    };
     popupmenu = {
       enabled = true;
       backend = "nui";
