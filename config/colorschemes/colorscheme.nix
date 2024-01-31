07ccb361fc471e9865eae6d1addd49661b38454a
Redyf <mateusalvespereira7@gmail.com>
2024-01-30 22:22:39 -0300

    feat: Add new integrations for catppuccin

--- config/colorschemes/colorscheme.nix
+++ config/colorschemes/colorscheme.nix
@@ -19,18 +19,33 @@
       disableUnderline = false;
       transparentBackground = true;
       integrations = {
+        cmp = true;
         noice = true;
         notify = true;
-        treesitter = true;
-        which_key = true;
-        cmp = true;
-        gitsigns = true;
-        harpoon = true;
         neotree = true;
-        mini.enabled = true;
-        indent_blankline.enabled = true;
-        native_lsp.enabled = true;
+        harpoon = true;
+        gitsigns = true;
+        which_key = true;
+        illuminate = {
+          enabled = true;
+        };
+        treesitter = true;
+        treesitter_context = true;
         telescope.enabled = true;
+        indent_blankline.enabled = true;
+        mini.enabled = true;
+        native_lsp = {
+          enabled = true;
+          inlay_hints = {
+            background = true;
+          };
+          underlines = {
+            errors = [ "underline" ];
+            hints = [ "underline" ];
+            information = [ "underline" ];
+            warnings = [ "underline" ];
+          };
+        };
       };
     };
   };
