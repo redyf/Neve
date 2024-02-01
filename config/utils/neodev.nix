2b3d13605352ea2d187c07a965c711fa39cb0a88
Redyf <mateusalvespereira7@gmail.com>
2024-02-01 20:37:45 -0300

    feat: Add neodev plugin

---
+++ config/utils/neodev.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+  extraPlugins = with pkgs.vimPlugins; [
+    neodev-nvim
+  ];
+
+  extraConfigLua = ''
+    require("neodev").setup({
+      library = { plugins = {"neotest"}, types = true},
+    })
+  '';
+}
