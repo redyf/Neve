3625066721be17fa1c2565752953133b4b3e82bd
Redyf <mateusalvespereira7@gmail.com>
2024-02-03 18:12:20 -0300

    feat: Add telescope-manix extension for Neve

--- config/completion/cmp.nix
+++ config/completion/cmp.nix
@@ -112,65 +112,65 @@
     cmp-cmdline = { enable = false; }; # autocomplete for cmdline
   };
   extraConfigLua = ''
-          luasnip = require("luasnip")
-          kind_icons = {
-            Text = "󰊄",
-            Method = "",
-            Function = "󰡱",
-            Constructor = "",
-            Field = "",
-            Variable = "󱀍",
-            Class = "",
-            Interface = "",
-            Module = "󰕳",
-            Property = "",
-            Unit = "",
-            Value = "",
-            Enum = "",
-            Keyword = "",
-            Snippet = "",
-            Color = "",
-            File = "",
-            Reference = "",
-            Folder = "",
-            EnumMember = "",
-            Constant = "",
-            Struct = "",
-            Event = "",
-            Operator = "",
-            TypeParameter = "",
-          } 
+      luasnip = require("luasnip")
+      kind_icons = {
+        Text = "󰊄",
+        Method = "",
+        Function = "󰡱",
+        Constructor = "",
+        Field = "",
+        Variable = "󱀍",
+        Class = "",
+        Interface = "",
+        Module = "󰕳",
+        Property = "",
+        Unit = "",
+        Value = "",
+        Enum = "",
+        Keyword = "",
+        Snippet = "",
+        Color = "",
+        File = "",
+        Reference = "",
+        Folder = "",
+        EnumMember = "",
+        Constant = "",
+        Struct = "",
+        Event = "",
+        Operator = "",
+        TypeParameter = "",
+      } 
 
-           local cmp = require'cmp'
+    local cmp = require'cmp'
 
-       -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-       cmp.setup.cmdline({'/', "?" }, {
-         sources = {
-           { name = 'buffer' }
-         }
-       })
+      -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({'/', "?" }, {
+          sources = {
+          { name = 'buffer' }
+          }
+          })
 
-      -- Set configuration for specific filetype.
-       cmp.setup.filetype('gitcommit', {
-         sources = cmp.config.sources({
-           { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-         }, {
-           { name = 'buffer' },
-         })
-       })
+    -- Set configuration for specific filetype.
+      cmp.setup.filetype('gitcommit', {
+          sources = cmp.config.sources({
+              { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+              }, {
+              { name = 'buffer' },
+              })
+          })
 
-       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-       cmp.setup.cmdline(':', {
-         sources = cmp.config.sources({
-           { name = 'path' }
-         }, {
-           { name = 'cmdline' }
-         }),
-    --      formatting = {
-    --       format = function(_, vim_item)
-    --         vim_item.kind = cmdIcons[vim_item.kind] or "FOO"
-    --       return vim_item
-    --      end
-    -- }
-       })  '';
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+          sources = cmp.config.sources({
+              { name = 'path' }
+              }, {
+              { name = 'cmdline' }
+              }),
+          --      formatting = {
+          --       format = function(_, vim_item)
+          --         vim_item.kind = cmdIcons[vim_item.kind] or "FOO"
+          --       return vim_item
+          --      end
+          -- }
+          })  '';
 }
