3ad8550064203affa7de9f088bec5209d64b7285
Redyf <mateusalvespereira7@gmail.com>
2024-02-03 00:59:41 -0300

    feat: Add fallback runner for neotest-java

--- config/utils/neotest.nix
+++ config/utils/neotest.nix
@@ -32,6 +32,18 @@
       adapters = {
         require("neotest-java")({
           ignore_wrapper = false,
+          -- function to determine which runner to use based on project path
+          determine_runner = function(project_root_path)
+            -- return should be "maven" or "gradle"
+            return "maven"
+          end,
+          -- override the builtin runner discovery behaviour to always use given
+          -- tool. Default is "nil", so no override
+          force_runner = nil,
+          -- if the automatic runner discovery can't uniquely determine whether
+          -- to use Gradle or Maven, fallback to using this runner. Default is
+          -- "maven"
+          fallback_runner = "gradle"
         }),
         require("neotest-python")({
         dap = { justMyCode = false },
