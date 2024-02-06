762fcb1e4138491d824906e8b75febb11431c13d
shard <sh7user@gmail.com>
2024-02-06 16:04:44 +0100

    theming change

--- config/colorschemes/colorscheme.nix
+++ config/colorschemes/colorscheme.nix
@@ -2,36 +2,6 @@
   colorschemes = {
     onedark = {
       enable = true;
-      transparentBackground = true;
-      integrations = {
-        cmp = true;
-        noice = true;
-        notify = true;
-        neotree = true;
-        harpoon = true;
-        gitsigns = true;
-        which_key = true;
-        illuminate = {
-          enabled = true;
-        };
-        treesitter = true;
-        treesitter_context = true;
-        telescope.enabled = true;
-        indent_blankline.enabled = true;
-        mini.enabled = true;
-        native_lsp = {
-          enabled = true;
-          inlay_hints = {
-            background = true;
-          };
-          underlines = {
-            errors = [ "underline" ];
-            hints = [ "underline" ];
-            information = [ "underline" ];
-            warnings = [ "underline" ];
-          };
-        };
-      };
     };
   };
 }
