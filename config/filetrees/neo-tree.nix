0b789d31e35ab1de71dbf5fbf7a75565a68e19c0
Redyf <mateusalvespereira7@gmail.com>
2024-02-06 20:55:04 -0300

    feat: Disable neo-tree plugin
    Disabling neo-tree plugin as I'm going to switch to oil.nvim for file navigation.

--- config/filetrees/neo-tree.nix
+++ config/filetrees/neo-tree.nix
@@ -1,6 +1,6 @@
 {
   plugins.neo-tree = {
-    enable = true;
+    enable = false;
     enableDiagnostics = true;
     enableGitStatus = true;
     enableModifiedMarkers = true;
@@ -23,42 +23,42 @@
     };
   };
 
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>e";
-      action = ":Neotree toggle reveal_force_cwd<cr>";
-      options = {
-        silent = true;
-        desc = "Explorer NeoTree (root dir)";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>E";
-      action = "<cmd>Neotree toggle<CR>";
-      options = {
-        silent = true;
-        desc = "Explorer NeoTree (cwd)";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>be";
-      action = ":Neotree buffers<CR>";
-      options = {
-        silent = true;
-        desc = "Buffer explorer";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>ge";
-      action = ":Neotree git_status<CR>";
-      options = {
-        silent = true;
-        desc = "Git explorer";
-      };
-    }
-  ];
+  # keymaps = [
+  #   {
+  #     mode = "n";
+  #     key = "<leader>e";
+  #     action = ":Neotree toggle reveal_force_cwd<cr>";
+  #     options = {
+  #       silent = true;
+  #       desc = "Explorer NeoTree (root dir)";
+  #     };
+  #   }
+  #   {
+  #     mode = "n";
+  #     key = "<leader>E";
+  #     action = "<cmd>Neotree toggle<CR>";
+  #     options = {
+  #       silent = true;
+  #       desc = "Explorer NeoTree (cwd)";
+  #     };
+  #   }
+  #   {
+  #     mode = "n";
+  #     key = "<leader>be";
+  #     action = ":Neotree buffers<CR>";
+  #     options = {
+  #       silent = true;
+  #       desc = "Buffer explorer";
+  #     };
+  #   }
+  #   {
+  #     mode = "n";
+  #     key = "<leader>ge";
+  #     action = ":Neotree git_status<CR>";
+  #     options = {
+  #       silent = true;
+  #       desc = "Git explorer";
+  #     };
+  #   }
+  # ];
 }
