7b7dfecb4996b1f5d07f023273c2fa82cbe7496c
Redyf <mateusalvespereira7@gmail.com>
2024-02-07 21:30:33 -0300

    feat: Change options for sidebar.nvim

--- config/utils/sidebar.nix
+++ config/utils/sidebar.nix
@@ -12,36 +12,33 @@
     })
   ];
   extraConfigLua = ''
-    local sidebar = require("sidebar-nvim")
-    opts = {
-      disable_default_keybindings = 0,
-      bindings = nil,
-      open = false,
-      side = "left",
-      initial_width = 32,
-      hide_statusline = false,
-      update_interval = 100,
-      sections = { "git", "containers" },
-      section_separator = { "", "-----", "" },
-      section_title_separator = { "" },
-      containers = {
-        attach_shell = "/bin/sh",
-        show_all = true,
-        interval = 5000,
-      },
-      -- datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
-      todos = { ignored_paths = { "~" } },
-      ["git"] = {
-        icon = "",
-      },
-    },
-      -- TODO: fix sidebar nvim
-      -- cmd = {
-      --   "SidebarNvimToggle",
-      --   "SidebarNvimOpen",
-      --   "SidebarNvimFocus",
-      --   "SidebarNvimUpdate",
-      -- },
+        local sidebar = require("sidebar-nvim")
+        sidebar.setup({
+        disable_default_keybindings = 0,
+        bindings = nil,
+        open = false,
+        side = "left",
+        initial_width = 32,
+        hide_statusline = false,
+        update_interval = 1000,
+        sections = { "git", "containers" },
+        section_separator = {"", "-----", ""},
+        section_title_separator = {""},
+        containers = {
+            attach_shell = "/bin/sh", show_all = true, interval = 5000,
+        },
+        datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
+        todos = { ignored_paths = {} },
+        ["git"] = {
+            icon = "", -- 
+        },
+    })
+          cmd = {
+            "SidebarNvimToggle",
+            "SidebarNvimOpen",
+            "SidebarNvimFocus",
+            "SidebarNvimUpdate",
+          },
   '';
 
   keymaps = [
