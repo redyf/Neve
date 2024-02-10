50563d05322940ef330d90ee667dc55b1e89b26e
Redyf <mateusalvespereira7@gmail.com>
2024-02-10 00:21:21 -0300

    feat: Add onAttach option for none-ls to avoid using lsp-format plugin

--- config/none-ls/none-ls.nix
+++ config/none-ls/none-ls.nix
@@ -1,8 +1,22 @@
 {
   plugins.none-ls = {
     enable = false;
-    enableLspFormat = true;
+    enableLspFormat = false;
     updateInInsert = false;
+    onAttach = ''
+      function(client, bufnr)
+          if client.supports_method "textDocument/formatting" then
+            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format { bufnr = bufnr }
+              end,
+            })
+          end
+        end
+    '';
     sources = {
       code_actions = {
         eslint_d.enable = true;
