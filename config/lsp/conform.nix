4d3cf7d75bfc0723bf7ca501b109dbbf81139096
Redyf <mateusalvespereira7@gmail.com>
2024-02-10 12:53:01 -0300

    feat: Disable autoformatting + add option to enable/disable formatting manually

--- config/lsp/conform.nix
+++ config/lsp/conform.nix
@@ -1,26 +1,57 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    formatOnSave = {
-      lspFallback = true;
-      timeoutMs = 500;
-    };
+    # As a shortcut, conform will optionally set up this format-on-save autocmd for you
+    # formatOnSave = {
+    #   lspFallback = true;
+    #   timeoutMs = 500;
+    # };
     notifyOnError = true;
     formattersByFt = {
-      html = [ [ "prettierd" "prettier" ] ];
-      css = [ [ "prettierd" "prettier" ] ];
-      javascript = [ [ "prettierd" "prettier" ] ];
-      javascriptreact = [ [ "prettierd" "prettier" ] ];
-      typescript = [ [ "prettierd" "prettier" ] ];
-      typescriptreact = [ [ "prettierd" "prettier" ] ];
-      java = [ "google-java-format" ];
-      python = [ "black" ];
-      lua = [ "stylua" ];
-      nix = [ "nixpkgs_fmt" ];
-      markdown = [ [ "prettierd" "prettier" ] ];
-      rust = [ "rustfmt" ];
+      html = [["prettierd" "prettier"]];
+      css = [["prettierd" "prettier"]];
+      javascript = [["prettierd" "prettier"]];
+      javascriptreact = [["prettierd" "prettier"]];
+      typescript = [["prettierd" "prettier"]];
+      typescriptreact = [["prettierd" "prettier"]];
+      java = ["google-java-format"];
+      python = ["black"];
+      lua = ["stylua"];
+      nix = ["alejandra"];
+      markdown = [["prettierd" "prettier"]];
+      rust = ["rustfmt"];
     };
   };
+
+  extraConfigLua = ''
+      require("conform").setup({
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
+    })
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+  '';
 
   # keymaps = [
   # {
