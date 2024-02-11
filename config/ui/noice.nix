ed7a925423f31d3d43b9c7ad5a3d7c70b9b9554e
Redyf <mateusalvespereira7@gmail.com>
2024-02-11 16:53:40 -0300

    feat: Add new options for noice

--- config/ui/noice.nix
+++ config/ui/noice.nix
@@ -20,5 +20,17 @@
       enabled = true;
       backend = "nui";
     };
+    format = {
+      filter = {
+        pattern = [":%s*%%s*s:%s*" ":%s*%%s*s!%s*" ":%s*%%s*s/%s*" "%s*s:%s*" ":%s*s!%s*" ":%s*s/%s*"];
+        icon = "";
+        lang = "regex";
+      };
+      replace = {
+        pattern = [":%s*%%s*s:%w*:%s*" ":%s*%%s*s!%w*!%s*" ":%s*%%s*s/%w*/%s*" "%s*s:%w*:%s*" ":%s*s!%w*!%s*" ":%s*s/%w*/%s*"];
+        icon = "󱞪";
+        lang = "regex";
+      };
+    };
   };
 }
