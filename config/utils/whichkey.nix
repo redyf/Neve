577a4096d73fa3d8fa94c5de58d697d2740b1c01
Redyf <mateusalvespereira7@gmail.com>
2024-02-12 13:41:03 -0300

    feat: Add new options for which key
    Interesting options that are available, however it's still a work in progress.

--- config/utils/whichkey.nix
+++ config/utils/whichkey.nix
@@ -1,5 +1,19 @@
 {
+  # TODO: Implement general mappings
   plugins.which-key = {
     enable = true;
+    ignoreMissing = false;
+    icons = {
+      breadcrumb = "»";
+      group = "+";
+      separator = ""; # ➜
+    };
+    # registrations = {
+    #   "<leader>t" = " Terminal";
+    # };
+    window = {
+      border = "none";
+      winblend = 0;
+    };
   };
 }
