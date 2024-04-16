722634a5e69c1e1e5e9502fc8810348ae0054be0
Andreas <49288593+asamonik@users.noreply.github.com>
2024-04-16 12:26:30 +0200

    catpuccin colorscheme renamed settings fix
    Signed-off-by: Andreas <49288593+asamonik@users.noreply.github.com>

--- config/colorschemes/catppuccin.nix
+++ config/colorschemes/catppuccin.nix
@@ -2,41 +2,43 @@
   colorschemes = {
     catppuccin = {
       enable = true;
-      background = {
-        light = "macchiato";
-        dark = "mocha";
-      };
-      flavour = "mocha"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
-      disableBold = false;
-      disableItalic = false;
-      disableUnderline = false;
-      transparentBackground = true;
-      integrations = {
-        cmp = true;
-        noice = true;
-        notify = true;
-        neotree = true;
-        harpoon = true;
-        gitsigns = true;
-        which_key = true;
-        illuminate = {
-          enabled = true;
+      settings = {
+        background = {
+          light = "macchiato";
+          dark = "mocha";
         };
-        treesitter = true;
-        treesitter_context = true;
-        telescope.enabled = true;
-        indent_blankline.enabled = true;
-        mini.enabled = true;
-        native_lsp = {
-          enabled = true;
-          inlay_hints = {
-            background = true;
+        flavour = "mocha"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
+        disableBold = false;
+        disableItalic = false;
+        disableUnderline = false;
+        transparentBackground = true;
+        integrations = {
+          cmp = true;
+          noice = true;
+          notify = true;
+          neotree = true;
+          harpoon = true;
+          gitsigns = true;
+          which_key = true;
+          illuminate = {
+            enabled = true;
           };
-          underlines = {
-            errors = ["underline"];
-            hints = ["underline"];
-            information = ["underline"];
-            warnings = ["underline"];
+          treesitter = true;
+          treesitter_context = true;
+          telescope.enabled = true;
+          indent_blankline.enabled = true;
+          mini.enabled = true;
+          native_lsp = {
+            enabled = true;
+            inlay_hints = {
+              background = true;
+            };
+            underlines = {
+              errors = ["underline"];
+              hints = ["underline"];
+              information = ["underline"];
+              warnings = ["underline"];
+            };
           };
         };
       };
