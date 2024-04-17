f3efaff11be35259d4f1f8eb6f83144ee333473c
Redyf <mateusalvespereira7@gmail.com>
2024-04-17 16:16:27 -0300

    fix: builder error
    error: could not format file
    /nix/store/nmkp9qgraazj3vaxsdfawsmisi6wkf07-init.lua: error parsing:
    error occurred while creating ast: unexpected token `<`. (starting from
    line 166, character 260 and ending on line 166, character 261).
    It was due to toggleterm's open_mapping keymapping, nixvim refactored
    the original way of changing the mapping key, this commit fixes it.

--- config/utils/toggleterm.nix
+++ config/utils/toggleterm.nix
@@ -4,25 +4,25 @@
     settings = {
       size = ''
         function(term)
-        if term.direction == "horizontal" then
-          return 15
-            elseif term.direction == "vertical" then
+          if term.direction == "horizontal" then
+            return 15
+              elseif term.direction == "vertical" then
             return vim.o.columns * 0.4
-            end
-            end
+          end
+        end
       '';
-      open_mapping = "<A-i>";
-      hideNumbers = true;
-      shadeTerminals = true;
-      startInInsert = true;
-      terminalMappings = true;
-      persistMode = true;
-      insertMappings = true;
-      closeOnExit = true;
+      open_mapping = "[[<A-i>]]";
+      hide_numbers = true;
+      shade_terminals = true;
+      start_in_insert = true;
+      terminal_mappings = true;
+      persist_mode = true;
+      insert_mappings = true;
+      close_on_exit = true;
       shell = "zsh";
       direction = "horizontal"; # 'vertical' | 'horizontal' | 'window' | 'float'
-      autoScroll = true;
-      floatOpts = {
+      auto_scroll = true;
+      float_opts = {
         border = "single"; # 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
         width = 80;
         height = 20;
@@ -30,9 +30,9 @@
       };
       winbar = {
         enabled = true;
-        nameFormatter = ''
+        name_formatter = ''
           function(term)
-          return term.name
+            return term.name
           end
         '';
       };
