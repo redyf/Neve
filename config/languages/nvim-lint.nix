8248e28f5081a32ab7dcac16fe3220884b2b9b0c
redyf <mateusalvespereira7@gmail.com>
2024-10-10 18:57:48 -0300

    feat: add new linters

--- config/languages/nvim-lint.nix
+++ config/languages/nvim-lint.nix
@@ -2,15 +2,20 @@
   plugins.lint = {
     enable = true;
     lintersByFt = {
-      nix = ["statix"];
-      lua = ["selene"];
-      python = ["flake8"];
-      javascript = ["eslint_d"];
-      javascriptreact = ["eslint_d"];
-      typescript = ["eslint_d"];
-      typescriptreact = ["eslint_d"];
-      json = ["jsonlint"];
-      java = ["checkstyle"];
+      c = [ "cpplint" ];
+      cpp = [ "cpplint" ];
+      go = [ "golangci-lint" ];
+      nix = [ "statix" ];
+      lua = [ "selene" ];
+      python = [ "flake8" ];
+      javascript = [ "eslint_d" ];
+      javascriptreact = [ "eslint_d" ];
+      typescript = [ "eslint_d" ];
+      typescriptreact = [ "eslint_d" ];
+      json = [ "jsonlint" ];
+      java = [ "checkstyle" ];
+      haskell = [ "hlint" ];
+      bash = [ "shellcheck" ];
     };
   };
 }
