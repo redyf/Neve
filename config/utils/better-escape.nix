66d9a871a8bae693b265160a6da5ceaefe4fd248
redyf <mateusalvespereira7@gmail.com>
2024-10-10 16:54:42 -0300

    fix: better-escape causing issues when using lazygit
    The cursor would escape lazygit's UI if you used j or k in a short
    period of time.

--- config/utils/better-escape.nix
+++ config/utils/better-escape.nix
@@ -3,6 +3,31 @@
     enable = true;
     settings = {
       timeout = 200;
+      default_mappings = false;
+      mappings = {
+        i = {
+          j = {
+            k = "<Esc>";
+            j = "<Esc>";
+          };
+        };
+        c = {
+          j = {
+            k = "<Esc>";
+            j = "<Esc>";
+          };
+        };
+        v = {
+          j = {
+            k = "<Esc>";
+          };
+        };
+        s = {
+          j = {
+            k = "<Esc>";
+          };
+        };
+      };
     };
   };
 }
