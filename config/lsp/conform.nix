5d8b368e947714f29076a72bd9baf5580a1c7d4f
redyf <mateusalvespereira7@gmail.com>
2024-10-10 16:56:56 -0300

    fix: conform-nvim not working as expected
    It wasn't recognizing the formatters, even when they were properly
    installed. It was due to the option names being deprecated, but nixvim
    didn't warn about it.

--- config/lsp/conform.nix
+++ config/lsp/conform.nix
@@ -2,54 +2,57 @@
   plugins.conform-nvim = {
     enable = true;
     settings = {
-      notifyOnError = true;
-      formattersByFt = {
-        html = [
-          [
-            "prettierd"
-            "prettier"
-          ]
-        ];
-        css = [
-          [
-            "prettierd"
-            "prettier"
-          ]
-        ];
-        javascript = [
-          [
-            "prettierd"
-            "prettier"
-          ]
-        ];
-        javascriptreact = [
-          [
-            "prettierd"
-            "prettier"
-          ]
-        ];
-        typescript = [
-          [
-            "prettierd"
-            "prettier"
-          ]
-        ];
-        typescriptreact = [
-          [
-            "prettierd"
-            "prettier"
-          ]
-        ];
+      notify_on_error = true;
+      # default_format_opts = {
+      #   lsp_format = "fallback";
+      # };
+      # format_after_save = {
+      #   lsp_format = "fallback";
+      # };
+      format_on_save = {
+        timeoutMs = 500;
+        lspFallback = true;
+      };
+      formatters_by_ft = {
+        html = {
+          __unkeyed-1 = "prettierd";
+          __unkeyed-2 = "prettier";
+          stop_after_first = true;
+        };
+        css = {
+          __unkeyed-1 = "prettierd";
+          __unkeyed-2 = "prettier";
+          stop_after_first = true;
+        };
+        javascript = {
+          __unkeyed-1 = "prettierd";
+          __unkeyed-2 = "prettier";
+          stop_after_first = true;
+        };
+        javascriptreact = {
+          __unkeyed-1 = "prettierd";
+          __unkeyed-2 = "prettier";
+          stop_after_first = true;
+        };
+        typescript = {
+          __unkeyed-1 = "prettierd";
+          __unkeyed-2 = "prettier";
+          stop_after_first = true;
+        };
+        typescriptreact = {
+          __unkeyed-1 = "prettierd";
+          __unkeyed-2 = "prettier";
+          stop_after_first = true;
+        };
         java = [ "google-java-format" ];
         python = [ "black" ];
         lua = [ "stylua" ];
-        nix = [ "alejandra" ];
-        markdown = [
-          [
-            "prettierd"
-            "prettier"
-          ]
-        ];
+        nix = [ "nixfmt" ];
+        markdown = {
+          __unkeyed-1 = "prettierd";
+          __unkeyed-2 = "prettier";
+          stop_after_first = true;
+        };
         rust = [ "rustfmt" ];
       };
     };
@@ -61,7 +64,16 @@
       key = "<leader>uf";
       action = ":FormatToggle<CR>";
       options = {
-        desc = "Toggle Format";
+        desc = "Toggle Format Globally";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>uF";
+      action = ":FormatToggle!<CR>";
+      options = {
+        desc = "Toggle Format Locally";
         silent = true;
       };
     }
@@ -85,45 +97,4 @@
       };
     }
   ];
-
-  extraConfigLua = ''
-    local conform = require("conform")
-    local notify = require("notify")
-
-    conform.setup({
-      format_on_save = function(bufnr)
-      -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
-        return { timeout_ms = 500, lsp_fallback = true }
-      end,
-    })
-
-    local function show_notification(message, level)
-      notify(message, level, { title = "conform.nvim" })
-    end
-
-    vim.api.nvim_create_user_command("FormatToggle", function(args)
-      local is_global = not args.bang
-      if is_global then
-        vim.g.disable_autoformat = not vim.g.disable_autoformat
-      if vim.g.disable_autoformat then
-        show_notification("Autoformat-on-save disabled globally", "info")
-      else
-        show_notification("Autoformat-on-save enabled globally", "info")
-      end
-      else
-        vim.b.disable_autoformat = not vim.b.disable_autoformat
-      if vim.b.disable_autoformat then
-        show_notification("Autoformat-on-save disabled for this buffer", "info")
-      else
-        show_notification("Autoformat-on-save enabled for this buffer", "info")
-        end
-      end
-    end, {
-      desc = "Toggle autoformat-on-save",
-      bang = true,
-    })
-  '';
 }
