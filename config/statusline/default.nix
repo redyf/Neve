8a208b42e3a2ab8eef1b0ca4491208cf6786955c
redyf <mateusalvespereira7@gmail.com>
2024-10-12 20:01:13 -0300

    refactor: make statusline modular

---
+++ config/statusline/default.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  config,
+  ...
+}:
+{
+  imports = [
+    ./lualine.nix
+    ./staline.nix
+  ];
+
+  options = {
+    statusline.enable = lib.mkEnableOption "Enable statusline module";
+  };
+  config = lib.mkIf config.dap.enable {
+    lualine.enable = lib.mkDefault true;
+    staline.enable = lib.mkDefault false;
+  };
+}
