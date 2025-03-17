77b7d9f0b3c18cde273aeb2b4d95600a80469750
Jan Uhlík <jan@uhlik.me>
2025-03-17 10:20:07 +0100

    Fix lsp module name

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -15,7 +15,7 @@
   options = {
     lsp.enable = lib.mkEnableOption "Enable lsp module";
   };
-  config = lib.mkIf config.dap.enable {
+  config = lib.mkIf config.lsp.enable {
     conform.enable = lib.mkDefault true;
     fidget.enable = lib.mkDefault true;
     lsp-nvim.enable = lib.mkDefault true;
