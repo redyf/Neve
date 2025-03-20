ccccc1d5e3e113de4ee46a2240a241668a1ee71f
redyf <mateusalvespereira7@gmail.com>
2025-03-19 23:33:30 -0300

    chore: update notify according to nixvim docs

--- config/ui/notify.nix
+++ config/ui/notify.nix
@@ -6,11 +6,13 @@
   config = lib.mkIf config.notify.enable {
     plugins.notify = {
       enable = true;
-      backgroundColour = "#000000";
-      fps = 60;
-      render = "default";
-      timeout = 500;
-      topDown = true;
+      settings = {
+        topDown = true;
+        backgroundColour = "#000000";
+        fps = 60;
+        render = "default";
+        timeout = 500;
+      };
     };
     keymaps = [
       {
