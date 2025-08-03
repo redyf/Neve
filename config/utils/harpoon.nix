7deb1d771ca926848013920323ff03487a82bff2
redyf <mateusalvespereira7@gmail.com>
2025-08-03 01:03:40 -0300

    chore: update flakes + harpoon

--- config/utils/harpoon.nix
+++ config/utils/harpoon.nix
@@ -7,17 +7,38 @@
     plugins.harpoon = {
       enable = true;
       enableTelescope = true;
-      keymapsSilent = true;
-      keymaps = {
-        addFile = "<leader>ha";
-        toggleQuickMenu = "<C-e>";
-        navFile = {
-          "1" = "<leader>hj";
-          "2" = "<leader>hk";
-          "3" = "<leader>hl";
-          "4" = "<leader>hm";
-        };
-      };
     };
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>ha";
+        action.__raw = "function() require'harpoon':list():add() end";
+      }
+      {
+        mode = "n";
+        key = "<C-e>";
+        action.__raw = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end";
+      }
+      {
+        mode = "n";
+        key = "<C-j>";
+        action.__raw = "function() require'harpoon':list():select(1) end";
+      }
+      {
+        mode = "n";
+        key = "<C-k>";
+        action.__raw = "function() require'harpoon':list():select(2) end";
+      }
+      {
+        mode = "n";
+        key = "<C-l>";
+        action.__raw = "function() require'harpoon':list():select(3) end";
+      }
+      {
+        mode = "n";
+        key = "<C-m>";
+        action.__raw = "function() require'harpoon':list():select(4) end";
+      }
+    ];
   };
 }
